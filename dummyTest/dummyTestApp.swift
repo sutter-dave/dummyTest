248171350f5db46740df10aa0f34a4ffdb30f67b
Dave Sutter <sutter@intransix.com>
2022-01-24 17:57:00 -0800

    Initial Commit

---
+++ dummyTest/dummyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyTestApp.swift
+//  dummyTest
+//
+//  Created by Dave Sutter on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct dummyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
